5295f27c29fccd59e2f6559c8fa54daa71cd5f46
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2024-04-28 22:58:24 +0100

    product microfrontend: cors config

--- product/webpack.config.js
+++ product/webpack.config.js
@@ -14,6 +14,11 @@ module.exports = (_, argv) => ({
   devServer: {
     port: 9001,
     historyApiFallback: true,
+    headers: {
+      "Access-Control-Allow-Origin": "http://localhost:9000",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    },
   },
 
   module: {
